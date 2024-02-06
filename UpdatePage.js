feac930cdb72ad17d3d92c6624405bd59d91feb0
yasmen24 <yasmensaleh24@gmail.com>
2024-02-06 08:43:33 +0300

    simple edit in footer&add function in updatepage

--- UpdatePage.js
+++ UpdatePage.js
@@ -1,5 +1,10 @@
 
-function redirectToPages() {
-  // Redirect to the desired page
-  window.location.href = "DesignerHomepage.html";
-}
+var btn=document.getElementsByClassName("btn")[0];
+console.log(btn);
+
+
+btn.addEventListener('submit', function(event) {
+  // Redirect to a specific page when the button is clicked
+  window.location.href = "DesignerHomepage.html"; 
+});
+
